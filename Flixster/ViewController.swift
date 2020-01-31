b32b3921b9f3a11aa799f6db5564b173ed98b110
Adib Thaqif <adibthaqifx@gmail.com>
2020-01-30 23:58:24 -0800

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Adib Thaqif on 1/30/20.
+//  Copyright © 2020 Adib Thaqif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
